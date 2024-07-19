76a9fd49f18f00e3e5904170bc66cc707ad341d2
anttiharju <antti@harju.io>
2024-07-19 22:06:39 +0300

    Revert "See if shellcheck catches issues in the action run"
    This reverts commit b8b5ae03199167df0899de760aca9e23ac2400a0.

--- scripts/install-lint.sh
+++ scripts/install-lint.sh
@@ -3,6 +3,6 @@
 version=$(cat .golangci-version)
 install_dir=$(go env GOPATH)/bin
 
-mkdir -p $install_dir
-cd $install_dir
+mkdir -p "$install_dir"
+cd "$install_dir" || exit
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b . "$version"
