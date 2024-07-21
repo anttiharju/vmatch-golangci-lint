ec79a67e56bad2a911021ee2e64e0168207e582a
anttiharju <antti@harju.io>
2024-07-21 20:58:43 +0300

    Make pre-commit hook warning more specific

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 make ci
-git diff --quiet || { echo "You have unstaged changes; commit contents might not pass 'make ci'."; exit 1; }
+git diff --name-only -- '*.sh' '*.go' || { echo "You have unstaged changes; commit contents might not pass 'make ci'."; exit 1; }
