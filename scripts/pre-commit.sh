0db2d9472b8968dba351e97c6e276d48548cd8cf
anttiharju <antti@harju.io>
2024-07-21 21:55:07 +0300

    Add typecheck to pre-commit hook

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 
 git diff --staged --name-only --exit-code -- '*.sh' || make shellcheck
 git diff --staged --name-only --exit-code -- '*.go' || make lint
+git diff --staged --name-only --exit-code -- '*.ts' || bun run typecheck
 
-git diff --name-only -- '*.sh' '*.go' || { echo "You have unstaged changes; commit contents might not pass 'make ci'."; exit 1; }
+git diff --name-only -- '*.sh' '*.go' '*.ts' || { echo "You have unstaged changes; commit contents might not pass 'make ci'."; exit 1; }
