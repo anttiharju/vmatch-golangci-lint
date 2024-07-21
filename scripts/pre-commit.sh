3eb3c56a254d12cb16fdc82409de48050348b200
anttiharju <antti@harju.io>
2024-07-21 21:32:10 +0300

    Make pre-commit hook even more granular

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-make ci
+git diff --staged --name-only --exit-code -- '*.sh' || make shellcheck
+git diff --staged --name-only --exit-code -- '*.go' || make lint
+
 git diff --name-only -- '*.sh' '*.go' || { echo "You have unstaged changes; commit contents might not pass 'make ci'."; exit 1; }
