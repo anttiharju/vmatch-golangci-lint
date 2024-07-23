f3a0d8288798f728a04ec9c46c62c9e24de3874a
anttiharju <antti@harju.io>
2024-07-23 20:01:49 +0300

    Fix pre-commit hook

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -5,4 +5,4 @@ git diff --staged --name-only --exit-code -- '*.sh' || make shellcheck
 git diff --staged --name-only --exit-code -- '*.go' || make lint
 git diff --staged --name-only --exit-code -- '*.ts' || bun run typecheck
 
-git diff --name-only -- '*.sh' '*.go' '*.ts' || { echo "You have unstaged changes; commit contents might not pass 'make ci'."; exit 1; }
+git diff --name-only --exit-code -- '*.sh' '*.go' '*.ts' || { echo "You have unstaged changes; commit contents might not pass 'make ci'."; exit 1; }
