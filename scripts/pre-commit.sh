bf289f9de4f443b5ea0d2fd73bf7ee96fa399667
anttiharju <antti@harju.io>
2024-07-23 20:13:04 +0300

    Switch pre-commit hook from bash to sh
    sh is supposedly faster.

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,8 +1,28 @@
-#!/bin/bash
-set -euo pipefail
+#!/bin/sh
+set -eu
 
-git diff --staged --name-only --exit-code -- '*.sh' || make shellcheck
-git diff --staged --name-only --exit-code -- '*.go' || make lint
-git diff --staged --name-only --exit-code -- '*.ts' || bun run typecheck
+changes_to() {
+    for filetype in "$@"; do
+        if git diff --name-only -- "$filetype" || git diff --cached --name-only -- "$filetype"; then
+            return 0
+        fi
+    done
+    return 1
+}
 
-git diff --name-only --exit-code -- '*.sh' '*.go' '*.ts' || { echo "You have unstaged changes; commit contents might not pass 'make ci'."; exit 1; }
+if changes_to "*.sh"; then
+    make shellcheck
+fi
+
+if changes_to "*.go"; then
+    make lint
+fi
+
+if changes_to "*.ts"; then
+    bun run typecheck
+fi
+
+git diff --name-only --exit-code -- '*.sh' '*.go' '*.ts' || {
+	echo "You have unstaged changes; commit contents might not pass 'make ci'."
+	exit 1
+}
