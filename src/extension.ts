4c3734b9b4b496b35afca0cc59746580dbabd2a0
anttiharju <antti@harju.io>
2024-08-02 20:24:29 +0300

    Find all .golangci-version files in workspace

--- src/extension.ts
+++ src/extension.ts
@@ -5,12 +5,24 @@ import * as path from "node:path";
 export const activate = (context: vscode.ExtensionContext) => {
 	const disposable = vscode.commands.registerCommand(
 		"bun-vscode-extension.helloworld",
-		() => {
+		async () => {
 			let workspacePath = "";
 			if (workspace.workspaceFolders?.length) {
-				workspacePath = workspace.workspaceFolders[0].uri.fsPath;
-				workspacePath = path.normalize(workspacePath);
-				window.showInformationMessage(workspacePath);
+				workspacePath = path.normalize(
+					workspace.workspaceFolders[0].uri.fsPath,
+				);
+				const files = await workspace.findFiles("**/.golangci-version");
+
+				// Exclude workspacePath and .golangci-version from all found files
+				const relativeFiles = files.map((file) => {
+					const relativePath = path.relative(workspacePath, file.fsPath);
+					const suffixLength = ".golangci-version".length;
+					const trimmedPath = relativePath.slice(0, -suffixLength);
+					return `/${trimmedPath}`;
+				});
+
+				const message = `${workspacePath},${relativeFiles.join(",")}`;
+				window.showInformationMessage(message);
 			}
 		},
 	);
