26e5140773ff83879563152fe52b5891ebea495f
anttiharju <antti@harju.io>
2024-08-04 20:36:26 +0300

    Different return

--- src/config/config.go
+++ src/config/config.go
@@ -8,10 +8,8 @@ type Config struct {
 }
 
 func NewConfig() *Config {
-	cfg := &Config{
+	return &Config{
 		InstallDir:      string(os.PathSeparator) + "bin", // TODO: switch bin to golangci-lint-updater
 		VersionFileName: ".golangci-version",
 	}
-
-	return cfg
 }
