4f04195062cad2a0fa0d7b33d4f2185491ea4a98
anttiharju <git@anttiharju.dev>
2025-01-12 13:27:31 +0200

    Add comments

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
 module github.com/anttiharju/vmatch
 
+// also update in brew formula, although could automate
 go 1.23.4
--- scripts/build.sh
+++ scripts/build.sh
@@ -8,4 +8,6 @@ else
     BUILDID="$SHA"
 fi
 
+# This needs to be maintained in the brew formula separately.
+# Not ideal, but once it's settled I don't expect it to change much.
 go build -C cmd/$APP_NAME -ldflags "-s -w -buildid=$BUILDID -X github.com/anttiharju/vmatch/pkg/exit.appName=$APP_NAME" -o "$(pwd)/$APP_NAME"
