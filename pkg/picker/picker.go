7872c7afe6c565c4abf6c51e51d4016ef43be025
anttiharju <git@anttiharju.dev>
2025-02-10 21:28:07 +0200

    Locate go.mod

--- pkg/picker/picker.go
+++ pkg/picker/picker.go
@@ -5,11 +5,15 @@ import (
 	"fmt"
 	"runtime"
 
+	"github.com/anttiharju/vmatch/pkg/filefinder"
 	"github.com/anttiharju/vmatch/pkg/linter"
 )
 
 func SelectWrapper(ctx context.Context, args []string) int {
 	if len(args) > 0 && args[0] == "go" {
+		filePath, _ := filefinder.Locate("go.mod")
+		fmt.Println("Found go.mod at", filePath)
+
 		version := "1.23.5"
 		fmt.Println("https://go.dev/dl/go" + version + "." + runtime.GOOS + "-" + runtime.GOARCH + ".tar.gz")
 
