fd8c1304dd77871a66acf7c7f12a5311f6dfbe95
anttiharju <git@anttiharju.dev>
2025-02-23 15:21:18 +0200

    Revert "Setup test for ShellCheck"
    This reverts commit aa7cb59a35cedf32e5bc2feef5261a455d9f39c0.

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ goversion="$1"
 goos="$2"
 goarch="$3"
 path="$4"
-mkdir -p $path
+mkdir -p "$path"
 
 url="https://go.dev/dl/go${goversion}.${goos}-${goarch}.tar.gz"
 curl -sL "$url" | tar -C "$path" --strip-components=1 -xz
